4bbec33565b49bc83c5580e2a03f1e9e17ecfd70
Gabriel Guarisa <gabrielguarisa@gmail.com>
2021-05-07 15:20:09 -0300

    Version 0.1.1

--- retack/__init__.py
+++ retack/__init__.py
@@ -1,5 +1,5 @@
 from retack.experiment import Experiment
 from retack.optimization import OptunaOptimizer
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["Experiment", "OptunaOptimizer"]
