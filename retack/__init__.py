da1fac4c4395cf36f56360671a1fd8244934f20c
Gabriel Guarisa <gabrielguarisa@gmail.com>
2021-05-19 15:37:33 -0300

    changing optimizers path

--- retack/__init__.py
+++ retack/__init__.py
@@ -1,5 +1,5 @@
 from retack.experiment import Experiment
-from retack.optimization import OptunaOptimizer
+from retack.optimization.optimizers import OptunaOptimizer
 
 __version__ = "0.1.41"
 __all__ = ["Experiment", "OptunaOptimizer"]
